11c695d9e42d2e04659abac28369cf42c2875587
Wouter van den Broeke <vandenbroekew@gmail.com>
2020-10-13 21:53:41 +0200

    Updated styling

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 #Delay between updates (default = 20 (ms))
-delay					= 20
+delay = 20
 #Width and height of the window (default = 450,450)
-width					= 450
-height					= 450
+width = 450
+height = 450
 #Font size (optimal = 1.0 for windows, 1.5 for linux)
-font_multiplier			= 1.5
+font_multiplier	= 1.5
 #Highlight rows and columns that are being checked (default = False, only recommended with high delay)
-enable_hightlight		= True
+enable_hightlight = True
 #Highlight border size of the current spot that is being checked (default = 6)
-hightlight_border_size	= 6
+hightlight_border_size = 6
 #Enable visualization of the backtracking algorithm (default = True)
 #Without visualization, delay will be 0 and no updates will be shown until the sudoku is solved
 visualize = True
