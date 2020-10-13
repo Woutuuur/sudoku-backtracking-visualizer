cc12f78ea3f9495342aaabf1771b5051eded839d
Wouter van den Broeke <vandenbroekew@gmail.com>
2020-10-13 21:52:42 +0200

    Updated config file with more accurate information.

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 #Delay between updates (default = 20 (ms))
-delay					= 100
+delay					= 20
 #Width and height of the window (default = 450,450)
-#All multiples of 150 are supported
 width					= 450
 height					= 450
 #Font size (optimal = 1.0 for windows, 1.5 for linux)
@@ -10,8 +9,9 @@ font_multiplier			= 1.5
 enable_hightlight		= True
 #Highlight border size of the current spot that is being checked (default = 6)
 hightlight_border_size	= 6
-#Enable visualization of the backtracking algorithm (defualt = True)
+#Enable visualization of the backtracking algorithm (default = True)
 #Without visualization, delay will be 0 and no updates will be shown until the sudoku is solved
 visualize = True
+
 #Example board:
 #8...9.5.19.6531.2.........91..98.7..6.3...29...2......2...1...5.....8...3....6...
